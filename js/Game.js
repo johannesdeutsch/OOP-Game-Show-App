0fd2ec52068f345d76b72e7f765aa5aa88342fc0
Johannes Deutsch <johannes.deutsch@gmx.at>
2021-11-05 09:49:12 +0100

    Step 1 - 5

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = ['keep your chin up', 'cool as a cucumber', 'once in a blue moon', 'every cloud has a silver lining', 'out of the woods'];
+        this.activePhrase = 'null';
+    };
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {};
+
+ };
